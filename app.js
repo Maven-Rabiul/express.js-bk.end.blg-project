ed5a45a9d32c7ab2feec9eb235b28e1b39be9d31
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-11-06 08:11:40 +0600

    app.js file created.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+import express from 'express';
+import apiRoutes from './routes/api.js'; // Importing the routes
+import config from './app/config/config.js'; // Import the config
 
+const app = express();
+const PORT = config.port; // Fixed port
+
+app.use(express.json()); // Middleware to parse JSON
+
+// Use the API routes from api.js
+app.use('/api/blogs', apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is Swimming on port ${PORT}`);
+});
