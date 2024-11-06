7b039ccb6cb94bbf513aeac89758887055f43f8e
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-11-06 08:28:33 +0600

    api.js created and updated.

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ import * as blogController from '../app/controller/blogController.js';
 
 const router = express.Router();
 
-// Define routes
+// Defining  routes
 router.post('/create-blog', blogController.createBlog); // Create a new blog
 router.get('/read-blog', blogController.readBlog); // Read a blog
 router.put('/update-blog', blogController.updateBlog); // Update a blog
